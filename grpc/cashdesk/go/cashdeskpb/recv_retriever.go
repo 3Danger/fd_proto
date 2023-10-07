818f786f3cb8aa8ea7715eefa6adf1cd530afae2
csamuro <magomedov.m25@wb.ru>
2023-10-07 12:18:17 +0200

    stage

--- grpc/cashdesk/go/cashdeskpb/recv_retriever.go
+++ grpc/cashdesk/go/cashdeskpb/recv_retriever.go
@@ -1,14 +1,32 @@
 package cashdeskpb
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"time"
+)
 
-type recvRetriever struct {
+type cashdeskStreamEventsClientWithRetry struct {
 	Cashdesk_StreamEventsClient
-	attempts   int
-	retryDelay time.Duration
+	cnf  Config
+	opts []grpc.DialOption
+}
+
+type Config struct {
+	Addr string
+
+	//reconnect&retry
+	Attempts   int
+	RetryDelay time.Duration
+}
+
+func NewCashdeskClientWithRetry(cnf *Config) (Cashdesk_StreamEventsClient, error) {
+
 }
 
 func NewRecvRetriever(cashDeskStreamClient Cashdesk_StreamEventsClient, attempts int, retryDelay time.Duration) Cashdesk_StreamEventsClient {
+
 	return &recvRetriever{
 		Cashdesk_StreamEventsClient: cashDeskStreamClient,
 
@@ -26,4 +44,31 @@ func (r *recvRetriever) Recv() (*StreamWorkstationEventsResponse, error) {
 		}
 	}
 	return response, err
+}
+
+func retrier(ctx context.Context, attempts int, delay time.Duration, f func(ctx context.Context) error) (err error) {
+	ticker := time.NewTicker(delay)
+	defer ticker.Stop()
+
+	for ; attempts > 0; attempts-- {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			if err = f(ctx); err == nil {
+				return nil
+			}
+		}
+	}
+	return err
+}
+
+func connect(ctx context.Context, serverAddr string, opts ...grpc.DialOption) error {
+	conn, err := grpc.Dial(serverAddr, opts...)
+	if err != nil {
+		return fmt.Errorf("dealing conn: %w", err)
+	}
+	client := NewCashdeskClient(conn)
+	stream, err := client.StreamEvents(ctx)
+
 }
